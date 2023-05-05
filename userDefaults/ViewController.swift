4acbb63d565df78b36e8a9697a03a7a29fe5a1bc
sandeepnayak07 <130977214+sandeepnayak07@users.noreply.github.com>
2023-05-05 15:45:57 +0530

    first commit

--- userDefaults/ViewController.swift
+++ userDefaults/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBOutlet weak var TextFieldLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let value = UserDefaults.standard.string(forKey: "name")//used for retreving data which is available in userdefault in textlabel
+        if value != nil {
+            TextLabel.text = "Hello, \(value!)"
+        } else {
+            TextLabel.text = "welcome"
+        }
+            
+   
     }
-
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        UserDefaults.standard.set(TextFieldLabel.text!, forKey: "name")
+        let alert = UIAlertController(title: "Saving", message: "Your data saved successfully", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default) {(action) in
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
